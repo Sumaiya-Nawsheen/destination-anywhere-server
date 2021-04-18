147b34f27a2d1f9f12607e621987e3f2b0c09dfd
Sumaiya-Nawsheen <sumaiya.naw252@gmail.com>
2021-04-18 11:49:12 +0600

    review & services

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+const  ObjectId  = require('mongodb').ObjectID;
+require('dotenv').config();
+
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.kypi0.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+  const reviewCollection = client.db("destination-anywhere").collection("reviews");
+
+  app.post ('/addReview', (req, res) => {
+    const newReview = req.body;
+    reviewCollection.insertOne(newReview)
+    .then(result =>{
+       res.send(result.insertedCount>0);
+    })
+  })
+
+  app.get('/showreviews', (req,res) => {
+    reviewCollection.find()
+    .toArray((err, items) => {
+      res.send(items)
+      
+    })
+  })
+
+
+
+  const serviceCollection = client.db("destination-anywhere").collection("services");
+  app.post ('/addService', (req, res) => {
+    const newService = req.body;
+    serviceCollection.insertOne(newService)
+    .then(result =>{
+       res.send(result.insertedCount>0);
+    })
+  })
+
+  app.get('/services', (req,res) => {
+    serviceCollection.find()
+    .toArray((err, items) => {
+      res.send(items)
+      
+    })
+  })
+
+  app.get('/dashboard/booking/:id', (req,res) => {
+    serviceCollection.find({_id: ObjectId (req.params.id)})
+    .toArray((err, items) => {
+      console.log(items)
+      res.send(items[0]);
+      
+    })
+  })
+
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
